ebf2bb50860271ca887fdaa8dfc629e892dce22e
Jack <26012107+JackTheFlap@users.noreply.github.com>
2025-02-20 16:59:47 +0000

    Removed plausible analytics (not setup correctly?) Added basic search Switch DDG to default search

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,15 @@ import "./global.css";
 function noSearchDefaultPageRender() {
   const app = document.querySelector<HTMLDivElement>("#app")!;
   app.innerHTML = `
+  <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; height: 100vh;">
+    <h1>Unduck</h1>
+    <div class="url-container">
+      <input type="text" class="search-input url-input" />
+      <button id="search-button" class="search-button">
+            <img src="/search.svg" alt="Search" />
+          </button>
+    </div>
+  </div>
     <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; height: 100vh;">
       <div class="content-container">
         <h1>Unduck</h1>
@@ -20,7 +29,12 @@ function noSearchDefaultPageRender() {
           </button>
         </div>
       </div>
+      <div class="extra-container">
+        <p>Originally created by <a href="https://x.com/theo">Theo</a>. Modified by <a href="https://jacktheflap.com">JackTheFlap</a>.</p>
+      </div>
       <footer class="footer">
+        <a href="https://jacktheflap.com" target="_blank">JackTheFlap</a>
+      •
         <a href="https://t3.chat" target="_blank">t3.chat</a>
         •
         <a href="https://x.com/theo" target="_blank">theo</a>
@@ -32,6 +46,7 @@ function noSearchDefaultPageRender() {
 
   const copyButton = app.querySelector<HTMLButtonElement>(".copy-button")!;
   const copyIcon = copyButton.querySelector("img")!;
+  const searchButton = app.querySelector<HTMLButtonElement>(".search-button")!;
   const urlInput = app.querySelector<HTMLInputElement>(".url-input")!;
 
   copyButton.addEventListener("click", async () => {
@@ -42,10 +57,45 @@ function noSearchDefaultPageRender() {
       copyIcon.src = "/clipboard.svg";
     }, 2000);
   });
+  searchButton.addEventListener("click", async () => {
+    const searchInputText =
+      app.querySelector<HTMLInputElement>(".search-input")!.value;
+    search(searchInputText);
+  });
 }
 
-const LS_DEFAULT_BANG = localStorage.getItem("default-bang") ?? "g";
+const LS_DEFAULT_BANG = localStorage.getItem("default-bang") ?? "ddg";
 const defaultBang = bangs.find((b) => b.t === LS_DEFAULT_BANG);
+
+function search(searchText: string) {
+  const searchUrl = getBangredirectUrlFromSearch(searchText);
+  if (!searchUrl) return;
+  window.location.href = searchUrl;
+}
+
+function getBangredirectUrlFromSearch(searchText: string) {
+  if (!searchText) {
+    return null;
+  }
+  const match = searchText.match(/!(\S+)/i);
+
+  const bangCandidate = match?.[1]?.toLowerCase();
+  const selectedBang = bangs.find((b) => b.t === bangCandidate) ?? defaultBang;
+
+  // Remove the first bang from the query
+  const cleanQuery = searchText.replace(/!\S+\s*/i, "").trim();
+
+  // Format of the url is:
+  // https://www.google.com/search?q={{{s}}}
+  const searchUrl = selectedBang?.u.replace(
+    "{{{s}}}",
+    // Replace %2F with / to fix formats like "!ghr+t3dotgg/unduck"
+    encodeURIComponent(cleanQuery).replace(/%2F/g, "/")
+  );
+  if (!searchUrl) return null;
+
+  return searchUrl;
+}
 
 function getBangredirectUrl() {
   const url = new URL(window.location.href);
